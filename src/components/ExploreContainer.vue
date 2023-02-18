7005c0927f44d5fc40b74c9cf2639b63aeb8ffc1
Mike Gester <mikegester.sabuga023@gmail.com>
2023-02-18 10:34:20 +0800

    removed ts lang in script setup

--- src/components/ExploreContainer.vue
+++ src/components/ExploreContainer.vue
@@ -1,11 +1,12 @@
 <template>
   <div id="container">
     <strong>{{ name }}</strong>
-    <p>Explore <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+    <p>Explore <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI
+        Components</a></p>
   </div>
 </template>
 
-<script setup lang="ts">
+<script setup>
 defineProps({
   name: String,
 });
