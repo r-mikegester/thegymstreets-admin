615194756c1372a3b63f8b721e92b7c378c75dff
Mike Gester <mikegester.sabuga023@gmail.com>
2023-03-23 10:30:14 +0800

    Added pinia integrations

--- src/main.js
+++ src/main.js
@@ -22,13 +22,16 @@ import '@ionic/vue/css/display.css';
 
 /* Theme variables */
 import './theme/variables.css';
-
+import "./components/BaseLayout.vue";
 import './assets/tailwind.css'
+import { createPinia } from 'pinia';
 
+const pinia = createPinia()
 const app = createApp(App)
   .use(IonicVue)
   .use(router);
 
 router.isReady().then(() => {
+  app.use(pinia)
   app.mount('#app');
 });
