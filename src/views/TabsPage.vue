4ffcc7ad2205f42ad1a393af6206e878937a184f
Mike Gester <mikegester.sabuga023@gmail.com>
2023-03-23 10:26:55 +0800

    fix issues within the tabs page that shows error

--- src/views/TabsPage.vue
+++ src/views/TabsPage.vue
@@ -1,44 +1,46 @@
 <template>
-  <ion-page>e
+  <ion-page id="main-content">
     <ion-tabs>
       <ion-router-outlet></ion-router-outlet>
       <ion-tab-bar slot="bottom">
-        <ion-tab-button tab="tab1" href="/tabs/tab1">
+        <ion-tab-button tab="Dashboard" href="/tabs/Dashboard">
           <ion-icon :icon="gridOutline" />
           <ion-label>Dashboard</ion-label>
         </ion-tab-button>
 
-        <ion-tab-button tab="tab2" href="/tabs/tab2">
+        <ion-tab-button tab="Workouts" href="/tabs/Workouts">
+          <ion-icon :icon="imagesOutline" />
+          <ion-label>Workouts</ion-label>
+        </ion-tab-button>
+
+        <ion-tab-button tab="Members" href="/tabs/Members">
           <ion-icon :icon="peopleOutline" />
           <ion-label>Members</ion-label>
         </ion-tab-button>
 
-        <ion-tab-button tab="tab3" href="/tabs/tab3">
+        <ion-tab-button tab="Profile" href="/tabs/Profile">
           <ion-icon :icon="personOutline" />
           <ion-label>Profile</ion-label>
         </ion-tab-button>
       </ion-tab-bar>
-
     </ion-tabs>
   </ion-page>
 </template>
 
 <script>
-import { IonTabBar, IonTabButton, IonTabs, IonLabel, IonIcon, IonPage, IonRouterOutlet } from '@ionic/vue';
-import { peopleOutline, personOutline, gridOutline } from 'ionicons/icons';
-import { defineComponent } from 'vue';
-export default defineComponent({
-  components: {
-    IonIcon,
-    IonTabs,
-    IonTabBar,
-    IonTabButton,
-    IonLabel,
-    IonPage,
-    IonRouterOutlet,
+import { IonPage, IonTabs, IonRouterOutlet, IonTabBar, IonTabButton, IonLabel, IonIcon, } from '@ionic/vue';
+
+import { gridOutline, imagesOutline, peopleOutline, personOutline } from 'ionicons/icons';
+
+export default {
+  components: { IonPage, IonTabs, IonRouterOutlet, IonTabBar, IonTabButton, IonLabel, IonIcon },
+  data() {
+    return {
+      gridOutline,
+      imagesOutline,
+      peopleOutline,
+      personOutline,
+    };
   },
-  setup() {
-    return { peopleOutline, personOutline, gridOutline };
-  }
-});
+};
 </script>
