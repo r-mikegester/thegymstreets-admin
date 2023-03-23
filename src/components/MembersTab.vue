51d087dbe287c35dda5de0d9679ea414b27d84ae
Mike Gester <mikegester.sabuga023@gmail.com>
2023-03-23 10:37:03 +0800

    Members Tab segment on top switching function fix

---
+++ src/components/MembersTab.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="tab_content min-h-full " v-show="title == selectedTitle">
+        <slot />
+    </div>
+</template>
+<script>
+import { inject } from 'vue';
+export default {
+    props: ['title'],
+    setup() {
+        const selectedTitle = inject('selectedTitle')
+
+        return {
+            selectedTitle,
+        }
+    },
+}
+</script>
---
+++ src/components/MembersWrap.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="mx-auto max-w-full">
+        <ul class="mb-3 list-none p-0 flex">
+            <li class=" tabs_header w-full mx-auto text-center py-5 text-xl cursor-pointer transition ease-in-out delay-75 bg-[#004aad] border-b-2 border-blue-900 active:bg-blue-600 selected:bg-blue-600 hover:bg-blue-700 "
+                v-for="title in tabTitles" :key="title" @click="selectedTitle = title"
+                :class="{ selected: title == selectedTitle }">
+                {{ title }}
+            </li>
+        </ul>
+        <slot />
+    </div>
+</template>
+<script>
+import { ref, provide } from 'vue';
+export default {
+    setup(props, { slots }) {
+        const tabTitles = ref(slots.default().map((tab) => tab.props.title))
+        const selectedTitle = ref(tabTitles.value[0])
+        provide('selectedTitle', selectedTitle)
+        return {
+            selectedTitle,
+            tabTitles,
+        }
+    },
+}
+</script>
