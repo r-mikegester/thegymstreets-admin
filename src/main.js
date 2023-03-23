1a72c8dc3246cd6ca41a1a9cd79d48b49802a8d5
Mike Gester <mikegester.sabuga023@gmail.com>
2023-03-23 11:42:02 +0800

    completely remove baselayout

--- src/main.js
+++ src/main.js
@@ -22,7 +22,6 @@ import '@ionic/vue/css/display.css';
 
 /* Theme variables */
 import './theme/variables.css';
-import "./components/BaseLayout.vue";
 import './assets/tailwind.css'
 import { createPinia } from 'pinia';
 
