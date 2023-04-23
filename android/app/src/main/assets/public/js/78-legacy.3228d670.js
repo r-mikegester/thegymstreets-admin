0a0b2ee10af29f37f336dbe6de23c144997b4aaa
Mike Gester <mikegester.sabuga023@gmail.com>
2023-04-23 18:50:10 +0800

    Co-authored-by: Aczell Bien Florencio <keenplify@users.noreply.github.com>

---
+++ android/app/src/main/assets/public/js/78-legacy.3228d670.js
@@ -0,0 +1,6 @@
+"use strict";(self["webpackChunkmyApp"]=self["webpackChunkmyApp"]||[]).push([[78],{6078:(e,t,n)=>{n.r(t),n.d(t,{startStatusTap:()=>a});var s=n(65),r=n(4074),o=n(6587);
+/*!
+ * (C) Ionic http://ionicframework.com - MIT License
+ */
+const a=()=>{const e=window;e.addEventListener("statusTap",(()=>{(0,s.wj)((()=>{const t=e.innerWidth,n=e.innerHeight,a=document.elementFromPoint(t/2,n/2);if(!a)return;const i=(0,r.a)(a);i&&new Promise((e=>(0,o.c)(i,e))).then((()=>{(0,s.Iu)((async()=>{i.style.setProperty("--overflow","hidden"),await(0,r.s)(i,300),i.style.removeProperty("--overflow")}))}))}))}))}}}]);
+//# sourceMappingURL=78-legacy.3228d670.js.map
