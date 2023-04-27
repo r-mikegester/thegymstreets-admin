a06cebd5c2e962d202395f383e6466bd7f958de0
Mike Gester <mikegester.sabuga023@gmail.com>
2023-04-28 07:59:05 +0800

    added modal to support adding workout

--- src/views/WorkoutsPage.vue
+++ src/views/WorkoutsPage.vue
@@ -3,13 +3,14 @@
     <ion-header class="bg-[#004aad]">
       <ion-toolbar>
         <ion-buttons slot="primary">
-          <ion-button>
+          <ion-button id="openModal">
             <Icon icon="solar:add-circle-broken" class="w-20 h-20" slot="icon-only" />
           </ion-button>
         </ion-buttons>
         <ion-title>Workouts Library</ion-title>
       </ion-toolbar>
-       <ion-searchbar animated="true" placeholder="Search" mode="ios" class="mx-auto h-16 text-white md:w-2/4 bg-[#004aad]" v-model="searchTerm"></ion-searchbar>
+      <ion-searchbar animated="true" placeholder="Search" mode="ios" class="mx-auto h-16 text-white md:w-2/4 bg-[#004aad]"
+        v-model="searchTerm"></ion-searchbar>
     </ion-header>
     <ion-content :fullscreen="true">
       <div class="max-w-screen-xl p-5 mx-auto">
@@ -23,14 +24,42 @@
               <p>{{ workout.description }}</p>
             </ion-label>
           </ion-item>
+        
         </ion-list>
+        <ion-modal
+      trigger="openModal"
+      :initial-breakpoint="0.25"
+      :breakpoints="[0, 0.25, 0.5, 0.75, 1.00]"
+      handle-behavior="cycle"
+      class="text-gray-900"
+    >
+     <ion-toolbar>
+        <ion-buttons slot="primary">
+          <ion-button id="openModal">
+            <Icon icon="ph:check-bold"  class="w-20 h-20" slot="icon-only" />
+          </ion-button>
+        </ion-buttons>
+        <ion-title> Add New Workout</ion-title>
+      </ion-toolbar>
+      <ion-content class="ion-padding">
+        <div class="ion-margin-top">
+          <ion-label>Click the handle above to advance to the next breakpoint.</ion-label>
+          <ion-item>
+            <ion-input v-model.trim="newWorkout.title" placeholder="Title" type="text" label="workoutTitle"></ion-input>
+          </ion-item>
+          <ion-item>
+            <ion-input v-model.trim="newWorkout.description" placeholder="Description" type="text" label="workoutDescription"></ion-input>
+          </ion-item>
+        </div>
+      </ion-content>
+    </ion-modal>
       </div>
     </ion-content>
   </ion-page>
 </template>
 
 <script>
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonButton, IonSearchbar, IonItem, IonList, IonLabel, IonThumbnail } from '@ionic/vue';
+
 import { defineComponent } from 'vue';
 import { createOutline } from 'ionicons/icons';
 import { Icon } from '@iconify/vue';
@@ -38,22 +67,10 @@ import 'swiper/css/pagination';
 import { FreeMode, Pagination } from 'swiper';
 import 'swiper/css/free-mode';
 import backend from '../config/axios';
-
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonButton, IonSearchbar, IonItem, IonList, IonLabel, IonThumbnail, IonInput, IonModal } from '@ionic/vue';
 export default defineComponent({
   components: {
-    IonPage,
-    IonHeader,
-    IonToolbar,
-    IonTitle,
-    IonContent,
-    IonSearchbar,
-    IonButtons,
-    IonButton,
-    Icon,
-    IonItem,
-    IonList,
-    IonLabel,
-    IonThumbnail,
+    IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonSearchbar, IonButtons, IonButton, Icon, IonItem, IonList, IonLabel, IonThumbnail, IonInput, IonModal
   },
   setup() {
     return {
@@ -65,6 +82,7 @@ export default defineComponent({
     return {
       workouts: [],
       searchTerm: '',
+      newWorkout: { title: '', description: '' },
     };
   },
   async mounted() {
@@ -80,6 +98,20 @@ export default defineComponent({
       return this.workouts.filter((workout) =>
         workout.title.toLowerCase().includes(this.searchTerm.toLowerCase())
       );
+    },
+  },
+  methods: {
+    async addWorkout() {
+      try {
+        const response = await backend.post('/v1/admin/workout-library', {
+          title: this.newWorkout.title,
+          description: this.newWorkout.description,
+        });
+        this.workouts.push(response.data.data);
+        this.newWorkout = { title: '', description: '' };
+      } catch (error) {
+        console.error(error);
+      }
     },
   },
 });
