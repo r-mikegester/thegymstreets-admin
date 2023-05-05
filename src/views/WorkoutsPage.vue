f5c84ed128f7914c12f4032631f0ffb1362b2d15
Mike Gester <mikegester.sabuga023@gmail.com>
2023-05-05 15:03:33 +0800

    added creation of workouts in the main page

--- src/views/WorkoutsPage.vue
+++ src/views/WorkoutsPage.vue
@@ -15,9 +15,9 @@
     <ion-content :fullscreen="true">
       <div class="max-w-screen-xl p-5 mx-auto">
         <ion-list lines="none">
-          <ion-item class="text-gray-900" v-for="(workout, index) in filteredWorkouts" :key="index">
+          <ion-item href="/workoutsView" class="text-gray-900" detail="true" v-for="(workout, index) in filteredWorkouts" :key="index">
             <ion-thumbnail slot="start">
-              <img alt="Silhouette of mountains" src="../assets/imgs/thumbnail.svg" />
+              <img alt="Silhouette of mountains" src="../assets/imgs/thumbnail.svg" class="rounded-xl" />
             </ion-thumbnail>
             <ion-label>
               <h2>{{ workout.title }}</h2>
@@ -28,7 +28,7 @@
         </ion-list>
         <ion-modal
       trigger="openModal"
-      :initial-breakpoint="0.25"
+      :initial-breakpoint="1.00"
       :breakpoints="[0, 0.25, 0.5, 0.75, 1.00]"
       handle-behavior="cycle"
       class="text-gray-900"
@@ -39,17 +39,88 @@
             <Icon icon="ph:check-bold"  class="w-20 h-20" slot="icon-only" />
           </ion-button>
         </ion-buttons>
-        <ion-title> Add New Workout</ion-title>
+        <ion-title>Add New Workout</ion-title>
       </ion-toolbar>
-      <ion-content class="ion-padding">
+      <ion-content class="ion-padding rounded-t-3xl">
         <div class="ion-margin-top">
-          <ion-label>Click the handle above to advance to the next breakpoint.</ion-label>
-          <ion-item>
-            <ion-input v-model.trim="newWorkout.title" placeholder="Title" type="text" label="workoutTitle"></ion-input>
-          </ion-item>
-          <ion-item>
-            <ion-input v-model.trim="newWorkout.description" placeholder="Description" type="text" label="workoutDescription"></ion-input>
-          </ion-item>
+          <div class="">
+                
+                <!-- Workout Title -->
+                <div class="relative flex items-center px-5 my-3 ">
+                    <span class="absolute">
+                        <Icon icon="ph:text-t-bold" class="w-7 h-7 mx-5 text-gray-300 dark:text-gray-500" />
+                    </span>
+                    <input type="text" required
+                        class="block w-full  py-4 text-gray-700 text-xl bg-white rounded-xl px-16 pr-5 dark:bg-white  focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40 border-2 border-gray-300 "
+                        placeholder="Title*" :clear-input="true">
+                </div>
+                <!-- Workout Description-->
+                <div class="relative flex items-center px-5 pt-2 pb-0 my-3">
+                    <span class="absolute">
+                        <Icon icon="bi:text-paragraph" class="w-7 h-7 mx-5 text-gray-300 dark:text-gray-500" />
+                    </span>
+                    <input type="text"
+                        class="block w-full px-16 pr-5 py-4 text-gray-700 text-xl bg-white rounded-xl dark:bg-white focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40 border-2 border-gray-300"
+                        placeholder="Description">
+                </div>
+                <!-- Last Name-->
+                <div class="relative flex items-center px-5 pt-2 pb-0 my-3">
+                    <span class="absolute">
+                        <Icon icon="typcn:arrow-repeat"  class="w-7 h-7 mx-5 text-gray-300 dark:text-gray-500" />
+                    </span>
+                    <input type="text" required
+                        class="block w-full px-16 pr-5 py-4 text-gray-700 text-xl bg-white rounded-xl dark:bg-white focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40 border-2 border-gray-300"
+                        placeholder="Reps*">
+                </div>
+                <!--Email-->
+                <div class="relative flex items-center px-5 pt-2 pb-0 my-3">
+                    <span class="absolute">
+                        <Icon icon="ant-design:flag-outlined"
+                            class="w-7 h-7 mx-5 text-gray-300 dark:text-gray-500" />
+                    </span>
+                    <input type="email" required
+                        class="block w-full px-16 pr-5 py-4 text-gray-700 text-xl bg-white rounded-xl dark:bg-white focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40 border-2 border-gray-300"
+                        placeholder="Sets*">
+                  </div>
+                <!--Phone-->
+                <div class="relative flex items-center px-5 pb-0 my-3">
+                    <span class="absolute">
+                        <Icon icon="fluent:time-picker-20-filled" class="w-7 h-7 mx-5 text-gray-300 dark:text-gray-500" />
+                    </span>
+                    <input type="number" required
+                        class="block w-full px-16 pr-5 py-4 text-gray-700 text-xl bg-white rounded-xl dark:bg-white focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40 border-2 border-gray-300"
+                        placeholder="Time*">
+                </div>
+
+                <!--Password-->
+                <div class="relative flex items-center px-5 pt-2 pb-0 my-3">
+                    <span class="absolute">
+                        <Icon icon="icon-park-outline:muscle" class="w-7 h-7 mx-5 text-gray-300 dark:text-gray-500" />
+                    </span>
+                    <input type="password" required
+                        class="block w-full px-16 pr-5 py-4 text-gray-700 text-xl bg-white rounded-xl dark:bg-white focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40 border-2 border-gray-300"
+                        placeholder="Muscle Groups*">
+                </div>
+                <!-- Line 1-->
+                <div class="relative flex items-center px-5 my-3 ">
+                    <span class="absolute">
+                        <Icon icon="uil:image-share" class="w-7 h-7 mx-5 text-gray-300 dark:text-gray-500" />
+                    </span>
+                    <input type="text" required
+                        class="block w-full  py-4 text-gray-700 text-xl bg-white rounded-xl px-16 pr-5 dark:bg-white  focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40 border-2 border-gray-300 "
+                        placeholder="Image URL*" :clear-input="true">
+                </div>
+                   <!-- Line 1-->
+                   <div class="relative flex items-center px-5 my-3 ">
+                    <span class="absolute">
+                        <Icon icon="icon-park-outline:youtube" class="w-7 h-7 mx-5 text-gray-300 dark:text-gray-500" />
+                    </span>
+                    <input type="text" required
+                        class="block w-full  py-4 text-gray-700 text-xl bg-white rounded-xl px-16 pr-5 dark:bg-white  focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40 border-2 border-gray-300 "
+                        placeholder="Youtube URL*" :clear-input="true">
+                </div>
+            
+            </div>
         </div>
       </ion-content>
     </ion-modal>
@@ -67,10 +138,10 @@ import 'swiper/css/pagination';
 import { FreeMode, Pagination } from 'swiper';
 import 'swiper/css/free-mode';
 import backend from '../config/axios';
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonButton, IonSearchbar, IonItem, IonList, IonLabel, IonThumbnail, IonInput, IonModal } from '@ionic/vue';
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonButtons, IonButton, IonSearchbar, IonItem, IonList, IonLabel, IonThumbnail, IonModal } from '@ionic/vue';
 export default defineComponent({
   components: {
-    IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonSearchbar, IonButtons, IonButton, Icon, IonItem, IonList, IonLabel, IonThumbnail, IonInput, IonModal
+    IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonSearchbar, IonButtons, IonButton, Icon, IonItem, IonList, IonLabel, IonThumbnail, IonModal
   },
   setup() {
     return {
