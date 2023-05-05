1ff209cae9b962e7b889f534facb28d75df964c1
Mike Gester <mikegester.sabuga023@gmail.com>
2023-05-05 14:28:50 +0800

    Updated the Chart Colors, Values and Design

--- src/components/ChartsJS.vue
+++ src/components/ChartsJS.vue
@@ -1,64 +1,103 @@
 <template>
-<div class="hello">
-    <h1>{{ msg }}</h1>
-    <canvas id="myChart" width="400" height="200" >
-    <p>Hello Fallback World</p>
-</canvas>
- 
-</div>
+    <div class="hello">
+        <h1>{{ msg }}</h1>
+        <canvas id="myChart" width="400" height="200">
+            <p>Hello Fallback World</p>
+        </canvas>
+
+    </div>
 </template>
 <script>
+import { defineComponent } from 'vue';
 import Chart from 'chart.js/auto';
 import backend from '../config/axios'
- export default {
+export default defineComponent({
+
     name: "HelloWorld",
     props: {
         msg: String
     },
-    async mounted(){
+    
+
+    async mounted() {
         const result = await backend.get('/v1/admin/analytics')
         console.log(result)
         console.log("component mounted.")
         const ctx = document.getElementById('myChart');
         Chart.defaults.color = '#fff';
-     const myChart = new Chart(ctx, {
+        const myChart = new Chart(ctx, {
             type: 'line',
             data: {
                 labels: result.data.data.labels,
                 datasets: [{
                     label: 'No. of Clients Each Month',
                     data: result.data.data.clientValues,
-                    backgroundColor: 'rgba(255, 238, 0, 0.50)',
-                    borderColor: 'rgba(255, 238, 0, 100)',
-                    color:'#000',
-                    borderWidth: 3,
+                    borderColor: 'rgb(203, 255, 0,  0.75)',
+                    color: '#000',
+                    borderWidth: 5,
                     fill: true,
                     borderJoinStyle: 'round',
-                    pointBorderWidth: 0.1, 
+                    pointBorderWidth: 0,
                     tension: 0.5,
-                    
+                    backgroundColor: (ctx) => {
+                        const canvas = ctx.chart.ctx;
+                        const gradient = canvas.createLinearGradient(0, 0, 0, 600);
+
+                        gradient.addColorStop(0, 'rgb(203, 255, 0,  0.20)');
+                        gradient.addColorStop(1, 'rgb(255, 199, 0,  0.75)');
+            
+
+                        return gradient;
+                    },
+
                 },
                 {
                     label: 'No. of Subscriptions Each Month',
                     data: result.data.data.subscriptionValues,
-                    backgroundColor: 'rgba(255, 255, 255, 0.60)',
-                    borderColor: '#ffffff',
-                    color:'#fff',
-                    borderWidth: 3,
+                    borderColor: 'rgb(0, 255, 131,100)',
+                    color: '#fff',
+                    borderWidth: 5,
                     borderJoinStyle: 'round',
-                    fill: 'origin',
-                    pointBorderWidth: 0.1,                    
+                    fill: true,
+                    pointBorderWidth: 0,
                     tension: 0.5,
-                    
+                    backgroundColor: (ctx) => {
+                        const canvas = ctx.chart.ctx;
+                        const gradient = canvas.createLinearGradient(0, 0, 0, 800);
+
+                        gradient.addColorStop(1, 'rgb(0, 157, 255, 0.20)');
+                        gradient.addColorStop(0, 'rgb(0, 255, 131, 0.75)');
+            
+
+                        return gradient;
+                    },
                 },
-            ]
-                
+                ],
+
             },
 
             options: {
-                scale: {
+                responsive: true,
+                scales: {
+                    x: {
+                        grid: {
+                            display: false,
+                            
+                           
+                        }
+                    },
                     y: {
-                        beginAtZero: true
+                        beginAtZero: true,
+                        grid: {
+                            display: false,
+                        }
+                    }
+                },
+                plugins: {
+                    legend: {
+                        labels: {
+                            usePointStyle: true,
+                        },
                     }
                 }
             }
@@ -67,5 +106,5 @@ import backend from '../config/axios'
 
 
     }
- }
+});
 </script>
